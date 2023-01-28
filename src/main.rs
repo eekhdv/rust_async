f0a109ef3307e5829dbf679ece550608e9d5a497
khadievedem <khadiev.edem@gmail.com>
2023-01-28 23:06:37 +0300

    change std TcpListener to tokio's

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
 use std::fs;
 use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
 use tokio::time;
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
     // Listen for incoming TCP connections on localhost port 7878
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").await?;
 
     // Block forever, handling each request that arrives at this IP address
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream).await;
+        handle_connection(stream).await?;
     }
 }
 
 async fn handle_connection(mut stream: TcpStream) {
     // Read the first 1024 bytes of data from the stream
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
@@ -40,6 +39,6 @@ async fn handle_connection(mut stream: TcpStream) {
     // Write response back to the stream,
     // and flush the stream to ensure the response is sent back to the client
     let response = format!("{status_line}{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
