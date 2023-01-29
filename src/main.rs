03cdb13013b869d64ab4210a0005bb89d2110d3e
khadievedem <khadiev.edem@gmail.com>
2023-01-29 16:10:23 +0300

    create mod

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::fs;
-use tokio::io::{AsyncWriteExt, AsyncReadExt};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::time;
+mod web_server;
+
+use tokio::net::TcpListener;
+use web_server::handlers;
 
 #[tokio::main]
 async fn main() {
@@ -11,42 +11,8 @@ async fn main() {
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         tokio::task::spawn(async move {
-            handle_connection(socket).await;
+            handlers::con_handler(socket).await;
         });
     }
-
-
-    // Block forever, handling each request that arrives at this IP address
-    // for stream in listener.incoming() {
-    //     
-    //     let stream = stream.unwrap();
-
-    //     handle_connection(stream).await?;
-    // }
 }
 
-async fn handle_connection(mut stream: TcpStream) {
-    // Read the first 1024 bytes of data from the stream
-    let mut buffer = Vec::with_capacity(1024);
-    stream.read_buf(&mut buffer).await.unwrap();
-
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
-
-    // Respond with greetings or a 404,
-    // depending on the data in the request
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "src/hello.html")
-    } else if buffer.starts_with(sleep) {
-        time::sleep(time::Duration::from_secs(5)).await;
-        ("HTTP/1.1 200 OK\r\n\r\n", "src/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "src/404.html")
-    };
-    let contents = fs::read_to_string(filename).unwrap();
-
-    // Write response back to the stream,
-    // and flush the stream to ensure the response is sent back to the client
-    let response = format!("{status_line}{contents}");
-    stream.write_all(response.as_bytes()).await.unwrap();
-}
