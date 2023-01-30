d2b9cad7032ff33d5c4fc4f981b51c15dea35505
khadievedem <khadiev.edem@gmail.com>
2023-01-30 15:55:45 +0300

    initial app to send notifications

--- src/main.rs
+++ src/main.rs
@@ -3,49 +3,45 @@
 
 // use tokio::net::TcpListener;
 
+use std::collections::HashMap;
+use std::error::Error;
+
 use tokio;
-use wayland_client::{protocol::wl_registry, Connection, Dispatch, QueueHandle};
 
-struct AppData;
-
-impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
-    fn event(
-        _state: &mut Self,
-        _: &wl_registry::WlRegistry,
-        event: wl_registry::Event,
-        _: &(),
-        _: &Connection,
-        _: &QueueHandle<AppData>,
-    ) {
-        // When receiving events from the wl_registry, we are only interested in the
-        // `global` event, which signals a new available global.
-        // When receiving this event, we just print its characteristics in this example.
-        if let wl_registry::Event::Global { name, interface, version } = event {
-            println!("[{}] {} (v{})", name, interface, version);
-        }
-    }
-}
+use zbus::{zvariant::Value, Connection};
 
 #[tokio::main]
-async fn main() {
-    let conn = Connection::connect_to_env().expect("[ERROR] Cannot connect to the Wayland server!");
-    let display = conn.display();
+async fn main() -> Result<(), Box<dyn Error>> {
+    let conn = Connection::session().await?;
 
-    let mut event_queue = conn.new_event_queue();
-    
-    let qh = event_queue.handle();
-
-    let _registry = display.get_registry(&qh, ());
-
-
-
-    event_queue.roundtrip(&mut AppData).unwrap();
-    // let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
-    // loop {
-    //     let (socket, _) = listener.accept().await.unwrap();
-    //     tokio::task::spawn(async move {
-    //         handlers::con_handler(socket).await;
-    //     });
-    // }
+    let m = conn.call_method(
+        Some("org.freedesktop.Notifications"),
+        "/org/freedesktop/Notifications",
+        Some("org.freedesktop.Notifications"),
+        "Notify",
+        &(
+            "test-app",
+            0u32,
+            "dialog-information",
+            "Test title",
+            "Test body, teeeest body",
+            vec![""; 0],
+            HashMap::<&str, &Value>::new(),
+            5000,
+        ),
+    ).await?;
+    let reply: u32 = m.body().unwrap();
+    dbg!(reply);
+    Ok(())
 }
 
+// #[tokio::main] // ----------- web_server
+// async fn main() {
+// let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
+// loop {
+//     let (socket, _) = listener.accept().await.unwrap();
+//     tokio::task::spawn(async move {
+//         handlers::con_handler(socket).await;
+//     });
+// }
+// }
