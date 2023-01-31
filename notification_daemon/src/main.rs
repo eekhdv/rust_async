7e47c38030489008d2e2cbab3c75c598a79be12c
khadievedem <khadiev.edem@gmail.com>
2023-02-01 02:42:00 +0300

    rm qt stuff

--- notification_daemon/src/main.rs
+++ notification_daemon/src/main.rs
@@ -1,17 +1,11 @@
 mod dbus;
 use dbus::service;
 
-// mod qt;
-// use qt::notif_widget::NotificationWidget;
-
 use std::error::Error;
 
 use tokio;
 use tokio::sync::mpsc;
 use uuid::Uuid;
-
-// use qt_widgets::QApplication;
-// use qt_core;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -19,21 +13,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let (tx, mut rx) = mpsc::channel(uuid::Uuid::new_v4().to_u128_le() % usize::MAX);
 
-    // loop {
-    //     std::future::pending::<()>().await;
-    // }
+    loop {
+        std::future::pending::<()>().await;
+    }
     
-    // unsafe {qt_core::QCoreApplication::set_attribute_1a(qt_core::ApplicationAttribute::AAShareOpenGLContexts)};
-    // QApplication::init(|_| {
-
-    //     let test = NotificationWidget::new();
-
-    //     test.show();
-
-    //     unsafe {
-    //         QApplication::exec()
-    //     }
-    // });
     Ok(())
 }
 
