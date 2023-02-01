62e123e78e84428f83ca5bd3c7f6850a7ea6aabf
khadievedem <khadiev.edem@gmail.com>
2023-02-01 08:55:57 +0300

    add task for receiving notifications

--- notification_daemon/src/main.rs
+++ notification_daemon/src/main.rs
@@ -1,22 +1,31 @@
 mod dbus;
-use dbus::service;
+use dbus::{prep_notifications, raw_handlers, service};
 
 use std::error::Error;
 
 use tokio;
 use tokio::sync::mpsc;
-use uuid::Uuid;
+// use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    service::setup_server().await?;
+    let (dbus_tx, mut dbus_rx) = mpsc::channel(128);
 
-    let (tx, mut rx) = mpsc::channel(uuid::Uuid::new_v4().to_u128_le() % usize::MAX);
+    let notif_handler = raw_handlers::NotificationsHandler { dbus_tx: (dbus_tx) };
+
+    service::setup_server(notif_handler).await?;
+
+    tokio::task::spawn(async move {
+        while let Some(n) = dbus_rx.recv().await {
+            match n {
+                prep_notifications::DbusChannel::Notify { notification } => {
+                    println!("{:?}", notification)
+                }
+            }
+        }
+    });
 
     loop {
         std::future::pending::<()>().await;
     }
-    
-    Ok(())
 }
-
