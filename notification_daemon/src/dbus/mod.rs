c7709f2c7978e430d5cf2aef4a55c0ff9473fe69
khadievedem <khadiev.edem@gmail.com>
2023-02-01 08:55:05 +0300

    rename mod's and structs

--- notification_daemon/src/dbus/mod.rs
+++ notification_daemon/src/dbus/mod.rs
@@ -1,2 +1,3 @@
-pub mod notifications;
+pub mod prep_notifications;
 pub mod service;
+pub mod raw_handlers;
--- notification_daemon/src/dbus/service.rs
+++ notification_daemon/src/dbus/service.rs
@@ -1,14 +1,16 @@
 use zbus::ConnectionBuilder;
 
-use super::notifications::Notifications;
+use crate::dbus::raw_handlers::NotificationsHandler;
 
-pub async fn setup_server() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn setup_server(
+    notification_handler: NotificationsHandler,
+) -> Result<(), Box<dyn std::error::Error>> {
     let srvc_name = "org.freedesktop.Notifications";
     let srvc_obj = "/org/freedesktop/Notifications";
 
     let _ = ConnectionBuilder::session()?
         .name(srvc_name)?
-        .serve_at(srvc_obj, Notifications)?
+        .serve_at(srvc_obj, notification_handler)?
         .build()
         .await?;
     Ok(())
