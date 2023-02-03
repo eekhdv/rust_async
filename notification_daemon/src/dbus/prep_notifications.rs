dad7ab85a8fc9b173c05b6edcd383ff02ffe31b6
khadievedem <khadiev.edem@gmail.com>
2023-02-03 17:42:25 +0300

    add rect struct

--- notification_daemon/src/dbus/prep_notifications.rs
+++ notification_daemon/src/dbus/prep_notifications.rs
@@ -2,6 +2,18 @@ pub enum DbusChannel {
     Notify { notification: Notification },
 }
 
+#[derive(Debug, Default, PartialEq, Clone)]
+pub struct Rect {
+    left_up: (i32, i32),
+    right_down: (i32, i32),
+}
+
+impl Rect {
+    fn new(left_up: (i32, i32), right_down: (i32, i32)) -> Self {
+        Self { left_up: (left_up), right_down: (right_down) }
+    }
+}
+
 #[derive(Debug)]
 pub struct Notification {
     pub app_name: String,
@@ -9,5 +21,6 @@ pub struct Notification {
     pub title: String,
     pub body: String,
     pub expire_timeout: i32,
+    pub window: Rect,
 }
 
