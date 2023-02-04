d094c0e1f014933a71650ae931df6fc76eec587b
khadievedem <khadiev.edem@gmail.com>
2023-02-04 03:41:48 +0300

    draw rectangles on new Notification

--- notification_daemon/src/main.rs
+++ notification_daemon/src/main.rs
@@ -1,5 +1,6 @@
 use console_engine::{pixel, Color, KeyCode, ConsoleEngine};
 mod dbus;
+use dbus::prep_notifications::Rect;
 use dbus::{prep_notifications, raw_handlers, service};
 
 use std::error::Error;
@@ -10,7 +11,6 @@ use tokio::sync::{mpsc, Mutex};
 
 #[derive(Debug, Clone)]
 struct NotificationsDrawer {
-    // notification_boxes: Rc<RefCell<Vec<prep_notifications::Notification>>>,
     notification_boxes: Arc<Mutex<Vec<prep_notifications::Notification>>>,
 }
 
@@ -69,38 +69,56 @@ async fn main() -> Result<(), Box<dyn Error>> {
     });
 
     tokio::task::spawn(async move {
-        let mut engine = console_engine::ConsoleEngine::init_fill(10).unwrap();
+        let mut engine = console_engine::ConsoleEngine::init_fill(1).unwrap();
         let cur_screen = ScreenDimensions::new(engine.get_width(), engine.get_height());
         loop {
+            engine.wait_frame(); // wait for next frame + capture inputs
+            engine.check_resize(); // resize the terminal if its size has changed
+            engine.clear_screen();
+
             let lock = notif_drawer_clone.lock().await;
+            let mut cur_x = 1;
+            let mut cur_y = 1;
+
+            engine.print(0, 0, format!("width: {}, height: {}", cur_screen.width, cur_screen.height).as_str());
             for _notif_box in lock.iter() {
-                engine.wait_frame(); // wait for next frame + capture inputs
-                engine.check_resize(); // resize the terminal if its size has changed
                                        // exit check
                 // if engine.is_key_pressed(KeyCode::Char('q')) {
                 //     break;
                 // }
-                engine.clear_screen();
-                engine.print(0, 0, format!("width: {}, height: {}", cur_screen.width, cur_screen.height).as_str());
+
+                // _notif_box.window = Rect::new((cur_x, cur_y), (cur_x + 20, cur_y + 7));
+                let app_name_len: i32 = _notif_box.app_name.len().try_into().unwrap();
+                let body_len: i32 = _notif_box.body.len().try_into().unwrap();
                 engine.rect(
-                    4,
-                    4,
-                    18,
-                    12,
+                    cur_x,
+                    cur_y,
+                    cur_x + 2 + {if app_name_len > body_len { app_name_len } else { body_len } }  + 2,
+                    cur_y + 6,
                     pixel::pxl('#'),
                 );
 
-                engine.rect(
-                    20,
-                    20,
-                    50,
-                    39,
-                    pixel::pxl('#'),
-                );
+                engine.print(cur_x + 2, cur_y + 1, _notif_box.app_name.as_str());
+                engine.print(cur_x + 2, cur_y + 3, _notif_box.title.as_str());
+                engine.print(cur_x + 2, cur_y + 5, _notif_box.body.as_str());
 
-                engine.print(4, 4 + (12 - 4) / 2, "hello");
+                cur_y += 8;
+
+                if cur_y >= cur_screen.height.try_into().unwrap() {
+                    cur_x += 30;
+                    cur_y = 1;
+                }
+                // engine.rect(
+                //     20,
+                //     20,
+                //     50,
+                //     39,
+                //     pixel::pxl('#'),
+                // );
+
                 engine.draw(); // draw the screen
             }
+            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
         }
     });
 
