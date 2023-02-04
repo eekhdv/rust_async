b36a5e08afa54d298408829b40307b6b5a472c20
khadievedem <khadiev.edem@gmail.com>
2023-02-04 21:57:43 +0300

    create fn to set notification's lifetime

--- notification_daemon/src/dbus/prep_notifications.rs
+++ notification_daemon/src/dbus/prep_notifications.rs
@@ -13,3 +13,21 @@ pub struct Notification {
     pub expire_timeout: i32,
     pub unique_id: u32,
 }
+
+pub fn set_notif_lifetime(id: u32, millis: u64) {
+    tokio::task::spawn(async move {
+        tokio::time::sleep(std::time::Duration::from_millis(millis))
+            .await;
+        let connection = zbus::Connection::session().await.unwrap();
+        let _ = connection
+            .call_method(
+                Some("org.freedesktop.Notifications"),
+                "/org/freedesktop/Notifications",
+                Some("org.freedesktop.Notifications"),
+                "CloseNotification",
+                &(id),
+            )
+            .await
+            .unwrap();
+    });
+}
