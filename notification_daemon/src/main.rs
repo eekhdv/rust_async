a2df7263c9ad7f13df7485748598ef8a0516cb35
khadievedem <khadiev.edem@gmail.com>
2023-02-04 04:09:42 +0300

    delete comments

--- notification_daemon/src/main.rs
+++ notification_daemon/src/main.rs
@@ -1,8 +1,5 @@
-use console_engine::rect_style::BorderStyle;
-use console_engine::{pixel, Color, ConsoleEngine, KeyCode};
-
 mod dbus;
-use dbus::prep_notifications::Rect;
+// use dbus::prep_notifications::Rect;
 use dbus::{prep_notifications, raw_handlers, service};
 
 use std::error::Error;
@@ -11,26 +8,21 @@ use std::sync::Arc;
 use tokio;
 use tokio::sync::{mpsc, Mutex};
 
+use console_engine::rect_style::BorderStyle;
+use console_engine::ConsoleEngine;
+
 #[derive(Debug, Clone)]
-struct NotificationsDrawer {
-    notification_boxes: Arc<Mutex<Vec<prep_notifications::Notification>>>,
+pub struct NotificationsDrawer {
+    pub notification_boxes: Arc<Mutex<Vec<prep_notifications::Notification>>>,
 }
 
-struct ScreenDimensions {
-    width: u32,
-    height: u32,
+pub struct ScreenDimensions {
+    pub width: u32,
+    pub height: u32,
 }
-
-// impl Iterator for NotificationsDrawer {
-//     type Item = prep_notifications::Notification;
-//
-//     fn next(&mut self) -> Option<Self::Item> {
-//         self.notification_boxes.pop()
-//     }
-// }
 
 impl NotificationsDrawer {
-    fn new() -> Self {
+    pub fn new() -> Self {
         NotificationsDrawer {
             notification_boxes: Arc::new(Mutex::new(vec![])),
         }
@@ -38,7 +30,7 @@ impl NotificationsDrawer {
 }
 
 impl ScreenDimensions {
-    fn new(w: u32, h: u32) -> Self {
+    pub fn new(w: u32, h: u32) -> Self {
         Self {
             width: w,
             height: h,
@@ -75,7 +67,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     });
 
     tokio::task::spawn(async move {
-        let mut engine = console_engine::ConsoleEngine::init_fill(1).unwrap();
+        let mut engine = ConsoleEngine::init_fill(1).unwrap();
         let cur_screen = ScreenDimensions::new(engine.get_width(), engine.get_height());
         loop {
             engine.wait_frame(); // wait for next frame + capture inputs
@@ -91,15 +83,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 0,
                 format!("width: {}, height: {}", cur_screen.width, cur_screen.height).as_str(),
             );
-            for _notif_box in lock.iter() {
-                // exit check
+            for notif_box in lock.iter() {
                 // if engine.is_key_pressed(KeyCode::Char('q')) {
                 //     break;
                 // }
 
-                // _notif_box.window = Rect::new((cur_x, cur_y), (cur_x + 20, cur_y + 7));
-                let app_name_len: i32 = _notif_box.app_name.len().try_into().unwrap();
-                let body_len: i32 = _notif_box.body.len().try_into().unwrap();
+                let app_name_len: i32 = notif_box.app_name.len().try_into().unwrap();
+                let body_len: i32 = notif_box.body.len().try_into().unwrap();
                 engine.rect_border(
                     cur_x,
                     cur_y,
@@ -117,9 +107,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     BorderStyle::new_light(),
                 );
 
-                engine.print(cur_x + 2, cur_y + 1, _notif_box.app_name.as_str());
-                engine.print(cur_x + 2, cur_y + 3, _notif_box.title.as_str());
-                engine.print(cur_x + 2, cur_y + 5, _notif_box.body.as_str());
+                engine.print(cur_x + 2, cur_y + 1, notif_box.app_name.as_str());
+                engine.print(cur_x + 2, cur_y + 3, notif_box.title.as_str());
+                engine.print(cur_x + 2, cur_y + 5, notif_box.body.as_str());
 
                 cur_y += 8;
 
@@ -127,14 +117,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     cur_x += 30;
                     cur_y = 1;
                 }
-                // engine.rect(
-                //     20,
-                //     20,
-                //     50,
-                //     39,
-                //     pixel::pxl('#'),
-                // );
-
                 engine.draw(); // draw the screen
             }
             tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
@@ -144,34 +126,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
     loop {
         std::future::pending::<()>().await;
     }
-
-    // main loop, be aware that you'll have to break it because ctrl+C is captured
-    // loop {
-    //     engine.wait_frame(); // wait for next frame + capture inputs
-    //     engine.check_resize(); // resize the terminal if its size has changed
-    //                            // exit check
-    //     if engine.is_key_pressed(KeyCode::Char('q')) {
-    //         break;
-    //     }
-    //     engine.clear_screen();
-    //     engine.print(0, 0, format!("{}", engine.get_screen().get_height()).as_str());
-    //     engine.rect(
-    //         4,
-    //         4,
-    //         18,
-    //         12,
-    //         pixel::pxl('#'),
-    //     );
-
-    //     engine.rect(
-    //         20,
-    //         20,
-    //         50,
-    //         39,
-    //         pixel::pxl('#'),
-    //     );
-
-    //     engine.print(4, 4 + (12 - 4) / 2, "hello");
-    //     engine.draw(); // draw the screen
-    // }
 }
